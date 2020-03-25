52da2de18b8ded2d9a0b3f10e77cb074fae2e30d
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-25 15:34:24 -0600

    Change creation to create review on specified shelter

--- spec/features/reviews/user_can_view_all_reviews_for_a_shelter_spec.rb
+++ spec/features/reviews/user_can_view_all_reviews_for_a_shelter_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe "as a user", type: :feature do
                                state: "CO",
                                zip: "80601")
 
-    review_1 = Review.create(title: "Found my best friend!",
+    review_1 = shelter_1.reviews.create(title: "Found my best friend!",
                              rating: "5",
                              content: "The staff was very friendly and allowe dme to take my time meeting with any dog I thought was a godo fit. They even allowed and encouraged me to bring my other dog to meet one of the pups I was interested in. This flexibility helped me make sure the dog I met with was a good fit. I've found my new best friend!",
                              picture: "https://www.pedigreedatabase.com/uploads/Reliya/images/13592736_10208042346674904_51006761307976618_n-1.jpg")
 
-    review_2 = Review.create(title: "All animals seemed very happy",
+    review_2 = shelter_1.reviews.create(title: "All animals seemed very happy",
                              rating: "5",
                              content: "All animals seemed very happy and well taken care of. The kennels/cages were clean and each had a comfortable looking bed and a full water dish. Upon arriving I immediately noticed several volunteers and staff walking dogs or playing with them in the play yards.")
 
