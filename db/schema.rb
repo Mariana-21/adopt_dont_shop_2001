591bf510a4abf4d972bb1a85ff0c69e01fe4eb9a
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-24 18:18:06 -0600

    Create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200322210806) do
+ActiveRecord::Schema.define(version: 20200325000459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20200322210806) do
     t.string "adoption_status"
     t.bigint "shelter_id"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.string "content"
+    t.string "picture"
   end
 
   create_table "shelters", force: :cascade do |t|
