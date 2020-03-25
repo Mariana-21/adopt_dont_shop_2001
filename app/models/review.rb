3dd3586421cbbb183f52cdbbace3058f24917f23
Mariana Cid <cidmariana5@gmail.com>
2020-03-24 21:23:34 -0600

    Made progress on user story 5 with database.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :shelter
+    validates_presence_of :title
+    validates_presence_of :rating
+    validates_presence_of :content
+    validates_presence_of :picture, allow_nil: true
 end
