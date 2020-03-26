706585a544d2dc36a56e19ced121b80f74b0a305
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-25 22:13:57 -0600

    Change page expectations to review content

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -27,9 +27,9 @@ RSpec.describe "as a user", type: :feature do
 
     expect(current_path).to eq("/shelters/#{shelter_1.id}")
 
-    expect(page).to have_content(review_1.title)
-    expect(page).to have_content(review_1.rating)
-    expect(page).to have_content(review_1.content)
-    expect(page).to have_css("img[src*='#{review_1.picture}']")
+    expect(page).to have_content("Found my best friend!")
+    expect(page).to have_content("5")
+    expect(page).to have_content("The staff was very friendly and allowed me to take my time meeting with any dog I thought was a good fit. They even allowed and encouraged me to bring my other dog to meet one of the pups I was interested in. This flexibility helped me make sure the dog I met with was a good fit. I've found my new best friend!")
+    expect(page).to have_css("img[src*='https://www.pedigreedatabase.com/uploads/Reliya/images/13592736_10208042346674904_51006761307976618_n-1.jpg']")
   end
 end
