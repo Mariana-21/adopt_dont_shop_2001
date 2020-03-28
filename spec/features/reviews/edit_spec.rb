3aaffe0c83cfb7a50650327b9c67700f0e500f12
Mariana Cid <cidmariana5@gmail.com>
2020-03-28 12:56:09 -0600

    Added it block to test pets add up in favorites.

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -37,4 +37,24 @@ RSpec.describe "as a user", type: :feature do
     expect(page).to have_content("The staff was very friendly and allowed me to take my time meeting with any dog I thought was a good fit. They even allowed and encouraged me to bring my other dog to meet one of the pups I was interested in. This flexibility helped me make sure the dog I met with was a good fit. I've found my new best friend!")
     expect(page).to have_css("img[src*='https://www.pedigreedatabase.com/uploads/Reliya/images/13592736_10208042346674904_51006761307976618_n-1.jpg']")
   end
+
+  it 'I can not edit a review without a title, rating, and/or content' do 
+    shelter_1 = Shelter.create(name: "Adams County Animal Shelter",
+                               address: "10705 Fulton St",
+                               city: "Brighton",
+                               state: "CO",
+                               zip: "80601")
+    review_1 = shelter_1.reviews.create(title: "Found my best friend!",
+                             rating: "5",
+                             content: "The staff was very friendly and allowe dme to take my time meeting with any dog I thought was a godo fit. They even allowed and encouraged me to bring my other dog to meet one of the pups I was interested in. This flexibility helped me make sure the dog I met with was a good fit. I've found my new best friend!",
+                             picture: "https://www.pedigreedatabase.com/uploads/Reliya/images/13592736_10208042346674904_51006761307976618_n-1.jpg")
+
+    visit "/shelters/#{shelter_1.id}" 
+
+    click_link("Edit Review")
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/#{review_1.id}/edit")
+
+    expect(page).to have_content("Review not updated: Required information missing.")
+    expect(page).to have_button('Update Review')
+  end 
 end
