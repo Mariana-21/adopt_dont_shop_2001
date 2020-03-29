d821c417f812142e9d14d66a20b33ca5b1cce378
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-29 16:52:50 -0600

    Edit update method to add pet to favorites pets hash. Add line to store the favorited pet in a session

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,11 @@
 class FavoritesController < ApplicationController
+  def update
+    pet = Pet.find(params[:pet_id])
+    favorite.add_pet(pet.id)
+    session[:favorite] = favorite.pets
+    
+    flash[:notice] = "You now have added #{pet.name} to your favorites!"
+    redirect_to "/pets/#{pet.id}"
+  end
 
-    def update
-        pet = Pet.find(params[:pet_id])
-        flash[:notice] = "You now have added #{pet.name} to your favorites!"
-        redirect_to '/pets'
-    end
-end 
 
