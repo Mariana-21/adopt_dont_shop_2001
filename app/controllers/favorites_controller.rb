4dc2b9467099ee88ef21b9170a06c07770b404c6
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-29 23:17:22 -0600

    Fix quantity variable in update method on favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
       pet = Pet.find(params[:pet_id])
       favorite.add_pet(pet.id)
       session[:favorite]
-      quantity = favorite.count_of(pet.id)
+      quantity = favorite.count_of
       flash[:notice] = "You now have added #{pet.name} to your favorites!"
       redirect_to '/pets'
   end
