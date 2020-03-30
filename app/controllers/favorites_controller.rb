12dca5101669b5b7a4b893149fa389914f6b8901
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-30 15:24:20 -0600

    Remove quantity from favorites update method. Add session assignment equal to favorite pets array. Add redirection to pet show page rather than index page.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,9 @@ class FavoritesController < ApplicationController
   def update
       pet = Pet.find(params[:pet_id])
       favorite.add_pet(pet.id)
-      session[:favorite]
-      quantity = favorite.count_of
+      session[:favorite] = favorite.pets
       flash[:notice] = "You now have added #{pet.name} to your favorites!"
-      redirect_to '/pets'
+      redirect_to "/pets/#{pet.id}"
   end
 
   def destroy
