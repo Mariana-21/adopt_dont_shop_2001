43346f7885b40b1b7076851e2f6ed5ee7c405a25
Kelsha_Darby <ohanaipo@gmail.com>
2020-03-31 17:19:04 -0600

    Add assertion to test that removed pet is no longer on favorites page

--- spec/features/favorites/destroy_spec.rb
+++ spec/features/favorites/destroy_spec.rb
@@ -94,16 +94,8 @@ describe "As as user" do
       click_link "Remove #{pet_3.name} From Favorites"
     end
 
+    expect(page).to_not have_content(pet_3.name)
     expect(current_path).to eq("/favorites")
     expect(page).to have_content("Favorites: 2")
   end
 end
-
-# As a visitor
-# When I have added pets to my favorites list
-# And I visit my favorites page ("/favorites")
-# Next to each pet, I see a button or link to remove that pet from my favorites
-# When I click on that button or link to remove a favorite
-# A delete request is sent to "/favorites/:pet_id"
-# And I'm redirected back to the favorites page where I no longer see that pet listed
-# And I also see that the favorites indicator has decremented by 1
