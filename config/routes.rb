24be8d4bd559300c789cb6354812e723f96bbb3a
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-01 15:19:12 -0600

    Change pet creation route to pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
-  get '/shelters/:shelter_id/pets/new', to: 'shelter_pets#new'
-  post '/shelters/:shelter_id/pets', to: 'shelter_pets#create'
+  get '/shelters/:shelter_id/pets/new', to: 'pets#new'
+  post '/shelters/:shelter_id/pets', to: 'pets#create'
   get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
 
   get '/pets', to: 'pets#index'
