733a3eedeed3c38c5402080cad0107fc44a1823e
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-01 16:32:20 -0600

    Edit review deletion route for readbility

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get '/shelters/:shelter_id/reviews/new', to: 'reviews#new'
   get '/shelters/:shelter_id/reviews/:review_id/edit', to: 'reviews#edit'
   patch '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#update'
-  delete '/shelters/:shelter_id/:review_id', to: 'reviews#destroy'
+  delete '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#destroy'
 
   get '/favorites', to: 'favorites#index'
   patch '/favorites/:pet_id', to: 'favorites#update'
