18945fb189e207269bb80104206b515043907c11
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-01 15:35:05 -0600

    Edit shelter id in reviews routes to be shelter_id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
     def edit
       @review = Review.find(params[:review_id])
-      @shelter = Shelter.find(params[:id])
+      @shelter = Shelter.find(params[:shelter_id])
     end
 
     def update
-      @shelter = Shelter.find(params[:id])
+      @shelter = Shelter.find(params[:shelter_id])
      review = Review.find(params[:review_id])
      review.update(review_params)
       if review.save
@@ -17,11 +17,11 @@ class ReviewsController < ApplicationController
     end
 
   def new
-    @shelter = Shelter.find(params[:id])
+    @shelter = Shelter.find(params[:shelter_id])
   end
 
   def create
-    @shelter = Shelter.find(params[:id])
+    @shelter = Shelter.find(params[:shelter_id])
 
     review = @shelter.reviews.create(review_params)
     if review.save
@@ -33,7 +33,7 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @shelter = Shelter.find(params[:id])
+    @shelter = Shelter.find(params[:shelter_id])
     Review.destroy(params[:review_id])
     redirect_to "/shelters/#{@shelter.id}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,24 @@ Rails.application.routes.draw do
   patch '/shelters/:shelter_id', to: 'shelters#update'
   delete '/shelters/:shelter_id', to: 'shelters#destroy'
 
-  get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
-
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
   post '/shelters/:shelter_id/pets', to: 'pets#create'
+  get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
+
   get '/pets', to: 'pets#index'
   get '/pets/:pet_id', to: 'pets#show'
   get '/pets/:pet_id/edit', to: 'pets#edit'
   patch '/pets/:pet_id', to: 'pets#update'
   delete '/pets/:pet_id', to: 'pets#destroy'
 
-  post '/shelters/:id', to: 'reviews#create'
-  get '/shelters/:id/new', to: 'reviews#new'
-  get '/shelters/:id/:review_id/edit', to: 'reviews#edit'
-  patch '/shelters/:id/:review_id', to: 'reviews#update'
-  delete '/shelters/:id/:review_id', to: 'reviews#destroy'
+  post '/shelters/:shelter_id', to: 'reviews#create'
+  get '/shelters/:shelter_id/new', to: 'reviews#new'
+  get '/shelters/:shelter_id/:review_id/edit', to: 'reviews#edit'
+  patch '/shelters/:shelter_id/:review_id', to: 'reviews#update'
+  delete '/shelters/:shelter_id/:review_id', to: 'reviews#destroy'
 
-  patch '/favorites/:pet_id', to: 'favorites#update'
   get '/favorites', to: 'favorites#index'
+  patch '/favorites/:pet_id', to: 'favorites#update'
   delete '/favorites/:pet_id', to: 'favorites#destroy'
   delete '/favorites', to: 'favorites#destroy'
 end
