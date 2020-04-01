37cdc1d279870f8c53286dd905f7e17abeef4edd
Mariana Cid <cidmariana5@gmail.com>
2020-04-01 17:45:15 -0600

    Refactor made to  show flash notice after submitting application.

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -7,10 +7,17 @@ class AdoptionsController < ApplicationController
 
     def create
         adoption = Adoption.create(adoption_params)
+        favorites = favorite.pets.map { |pet_id| Pet.find(pet_id.to_i) }
 
-        adoption.save 
-
-        redirect_to '/favorites'
+        adoption_pets = Pet.find(params[:adoptable_pet])
+        if adoption.save 
+            flash[:notice] = "Thank you for submitting your application!"
+            redirect_to '/favorites'
+            adoption_pets.each {|pet| favorite.remove_pet(pet.id.to_s)}
+        elsif !adoption.save
+            flash[:notice] = "Application not submitted. Missing one or more of the following fields: name, address, city, state, zip, phone or description"
+            render :new
+        end
     end
 
     private 
